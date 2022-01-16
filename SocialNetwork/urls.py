b66747da4e6e54b8c34205a11a945a18f871f601
Ezgi Pelin Gürkan <ezgi.pgurkanx@gmail.com>
2022-01-16 17:59:30 +0300

    add login&logout

--- SocialNetwork/urls.py
+++ SocialNetwork/urls.py
@@ -24,11 +24,15 @@ from personal.views import (
 
 from account.views import (
     register,
+    login_view,
+    logout_view,
 )
 
 urlpatterns = [ 
     path('', home_screen_view, name='home'),
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),    
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
     path('register/', register, name='register'),
 
 ]
