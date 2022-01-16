51d1a90ecf12cb80514518ba99d722f8a1f23fdf
Ezgi Pelin Gürkan <ezgi.pgurkanx@gmail.com>
2022-01-16 16:38:26 +0300

    Registration

--- SocialNetwork/urls.py
+++ SocialNetwork/urls.py
@@ -22,11 +22,17 @@ from personal.views import (
     home_screen_view
 )
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
+from account.views import (
+    register,
+)
+
+urlpatterns = [ 
     path('', home_screen_view, name='home'),
+    path('admin/', admin.site.urls),
+    path('register/', register, name='register'),
 
 ]
+
 if settings.DEBUG:
     urlpatterns+= static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
