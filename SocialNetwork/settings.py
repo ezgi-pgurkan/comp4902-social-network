2596d91afbc312eb303852f217c867cb93d9314f
Ezgi Pelin Gürkan <ezgi.pgurkanx@gmail.com>
2022-01-25 06:26:24 +0300

    Never gonna give you up

--- SocialNetwork/settings.py
+++ SocialNetwork/settings.py
@@ -146,6 +146,7 @@ USE_L10N = True
 
 USE_TZ = True
 
+DATA_UPLOAD_MAX_MEMORY_SIZE = 10485760 # 10mb = 10 * 1024 *1024
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
